dc1c903a74359a29f274b2ea92ea81952d38a9f3
Cliff Jurgensen <jivefox.002@gmail.com>
2018-10-23 17:35:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ return facts;
 function iLoveTheBeatles(n) {
   var array = [ ]
   do {
-    array = ['I love the Beatles!']
+    array.push ('I love the Beatles!')
     n++;
   } while (n < 15);
   return array()
