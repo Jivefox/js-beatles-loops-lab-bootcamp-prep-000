347b9a67b66065ba98545cc7cc209edf9531b4c0
Cliff Jurgensen <jivefox.002@gmail.com>
2018-10-23 15:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [ ];
+  for (let i = 0; i < 4; i++) {
+      empty.push (`${musicians[i]} plays ${instruments[i]}`);
+  }
+      return empty;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (facts[i]) {
+    facts.push (`${facts[i]} + '!!!'`);
+}
+return facts;
+}
+
+function iLoveTheBeatles(n) {
+  
+}
