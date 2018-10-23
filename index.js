e32b4f8fcb605b5395b78d32b700cf0b2a3fb6db
Cliff Jurgensen <jivefox.002@gmail.com>
2018-10-23 16:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0;
   while (i < facts.length) {
-    i = i++;
+    i++;
     facts[i] = facts[i] + '!!!'
 }
 return facts;
